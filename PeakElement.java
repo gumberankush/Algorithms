c58185c9d2ea492f64739e5a554b166b4541ca99
gumberankush <er.ankushgumber@gmail.com>
2021-11-06 13:38:18 -0700

    162. Find Peak Element
    A peak element is an element that is strictly greater than its neighbors.

---
+++ PeakElement.java
@@ -0,0 +1,40 @@
+class PeakElement {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        
+        int size = nums.length;
+        
+        if(size == 1){
+            return 0;
+        }
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(mid > 0 && mid < nums.length-1){
+                if(nums[mid] > nums[mid-1] &&
+                  nums[mid] > nums[mid+1]){
+                    return mid;
+                }else if(nums[mid] > nums[mid-1]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }else if(mid == 0){
+                if(nums[mid] > nums[mid+1]){
+                    return mid;
+                }else{
+                    return mid+1;
+                }
+            }else if(mid == size-1){
+                if(nums[size-1] > nums[size-2]){
+                    return size-1;
+                }else{
+                    return size-2;
+                }
+            }
+        }
+        return -1;
+    }
+}
