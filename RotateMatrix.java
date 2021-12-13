1ae917cc100a1629ba445482c3888d4e85803727
gumberankush <er.ankushgumber@gmail.com>
2021-12-12 21:30:12 -0800

    48. Rotate Image

---
+++ RotateMatrix.java
@@ -0,0 +1,28 @@
+class RotateMatrix {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverseEachRow(matrix);
+    }
+    
+    private void transpose(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    private void reverseEachRow(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+}
